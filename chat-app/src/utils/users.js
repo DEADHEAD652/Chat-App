f508b9585034c53395d9fa79958b4f1ba86c0c96
hamza <hamzajjmalik@gmail.com>
2022-10-27 23:37:36 +0500

    renderring messages

--- chat-app/src/utils/users.js
+++ chat-app/src/utils/users.js
@@ -1,70 +1,55 @@
 const users = []
-//adduser,removeuser,getUser,getuserinroom
-const addUser = ({
-    id,
-    username,
-    room
-}) => {
-    //Clean data 
+
+const addUser = ({ id, username, room }) => {
+    // Clean the data
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
-    //validate data 
+    // Validate the data
     if (!username || !room) {
-
-
         return {
-            error: 'username and room are required !'
+            error: 'Username and room are required!'
         }
     }
-    //checking for exsisting user
-    const exsistingUser = users.find((user) => {
-        room = room.trim().toLowerCase()
+
+    // Check for existing user
+    const existingUser = users.find((user) => {
         return user.room === room && user.username === username
-
     })
-    //validate username
-    if (exsistingUser) {
-        return {
 
-            error: 'username is already used'
+    // Validate username
+    if (existingUser) {
+        return {
+            error: 'Username is in use!'
         }
     }
 
-    //storing user
-    const user = {
-        id,
-        username,
-        room
-    }
+    // Store user
+    const user = { id, username, room }
     users.push(user)
-    return {
-        user
-    }
-
-
+    return { user }
 }
 
-const removeuser = (id) => {
-
+const removeUser = (id) => {
     const index = users.findIndex((user) => user.id === id)
 
     if (index !== -1) {
         return users.splice(index, 1)[0]
-
     }
 }
 
-
 const getUser = (id) => {
     return users.find((user) => user.id === id)
+}
 
+const getUsersInRoom = (room) => {
+    room = room.trim().toLowerCase()
+    return users.filter((user) => user.room === room)
 }
-const getUsersinRoom = (room) => {
-    return users.filter((user) => {
-        user.room === room
-    })
-}
-module.exports= {
-    addUser,getUser,getUsersinRoom,removeuser
+
+module.exports = {
+    addUser,
+    removeUser,
+    getUser,
+    getUsersInRoom
 }
